f4422ae963081f449e14547a2e63911f9f09fa99
rarufino27-alt <rarufino27@gmail.com>
2026-01-07 13:51:12 -0300

    Atualizar o js.dataManager.js

--- js/js.dataManager.js
+++ js/js.dataManager.js
@@ -1,28 +1,93 @@
 (function(){
 
-const read = (k,d)=>JSON.parse(localStorage.getItem(k)) ?? d;
-const write = (k,v)=>localStorage.setItem(k,JSON.stringify(v));
+const STORAGE_KEY = "rfdriver_data";
+
+function load(){
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
+    configuracoes: { tema: "dark" },
+    agendamentos: [],
+    caixaAtual: [],
+    caixaDiario: [],
+    carteiraSaldo: 0
+  };
+}
+
+function save(data){
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
 
 window.DataManager = {
 
-  get(key, def=[]){
-    return read(key, def);
+  /* ================= CONFIG ================= */
+  getTema(){
+    return this.getData().configuracoes.tema || "dark";
   },
 
-  set(key, value){
-    write(key, value);
+  setTema(tema){
+    const d = this.getData();
+    d.configuracoes.tema = tema;
+    save(d);
   },
 
-  add(key, item){
-    const data = read(key, []);
-    data.push(item);
-    write(key, data);
+  /* ================= CORE ================= */
+  getData(){
+    return load();
   },
 
-  remove(key, index){
-    const data = read(key, []);
-    data.splice(index,1);
-    write(key, data);
+  saveData(data){
+    save(data);
+  },
+
+  /* ================= AGENDAMENTOS ================= */
+  listarAgendamentos(){
+    return this.getData().agendamentos || [];
+  },
+
+  adicionarAgendamento(ag){
+    const d = this.getData();
+    d.agendamentos.push(ag);
+    save(d);
+  },
+
+  removerAgendamento(id){
+    const d = this.getData();
+    d.agendamentos = d.agendamentos.filter(a=>a.id!==id);
+    save(d);
+  },
+
+  /* ================= CAIXA ================= */
+  getCaixaAtual(){
+    return this.getData().caixaAtual || [];
+  },
+
+  salvarCaixaAtual(lista){
+    const d = this.getData();
+    d.caixaAtual = lista;
+    save(d);
+  },
+
+  fecharCaixa(registro){
+    const d = this.getData();
+    d.caixaDiario.push(registro);
+    d.caixaAtual = [];
+    d.carteiraSaldo += registro.saldo;
+    save(d);
+  },
+
+  listarCaixaDiario(){
+    return this.getData().caixaDiario || [];
+  },
+
+  /* ================= CARTEIRA ================= */
+  getSaldoCarteira(){
+    return this.getData().carteiraSaldo || 0;
+  },
+
+  limparCarteira(){
+    const d = this.getData();
+    d.carteiraSaldo = 0;
+    d.caixaDiario = [];
+    save(d);
   }
 
 };
